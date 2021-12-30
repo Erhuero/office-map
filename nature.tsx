3b4d625b4012d122a0c7d4548cd81a10dc8909e6
Constantin <constantin.chtanko@gmail.com>
2021-12-30 02:03:17 +0100

    remove tsx

--- nature.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="nature" tilewidth="32" tileheight="32" tilecount="135" columns="15">
- <image source="../../../Downloads/RPG Nature Tileset Autumn.png" width="480" height="288"/>
-</tileset>
