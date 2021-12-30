245d31e352b120fca2bc67c0fcf55c34ffc0295a
Constantin <constantin.chtanko@gmail.com>
2021-12-30 02:00:58 +0100

    house del

--- House.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="House" tilewidth="32" tileheight="32" tilecount="12" columns="3">
- <image source="../../../Downloads/CuteRPG/House.png" width="112" height="144"/>
-</tileset>
